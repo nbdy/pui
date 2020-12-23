c0ab38d8ac5658549934ebd2be43c25385312847
Pascal Eberlein <pascal@eberlein.io>
2020-12-23 16:24:59 +0100

    header only modules added stuff

--- modules/Clock/Clock.h
+++ modules/Clock/Clock.h
@@ -5,9 +5,38 @@
 #ifndef PUI_CLOCK_H
 #define PUI_CLOCK_H
 
+#include <Widget/Widget.h>
+#include <BaseModule/BaseModule.h>
 
-class Clock {
+class ClockWidget : public Widget {
+public:
+    ClockWidget(callbackFunction onClicked, callbackFunction onLongClicked):
+    topLeft(Vector2{0, 0}), bottomRight(Vector2{50, 100}), onClicked(onClicked), onLongClicked(onLongClicked),
+    {}
+};
 
+class Clock : public BaseModule {
+protected:
+    virtual void onWidgetClicked() {
+        // todo open module view
+    }
+
+    virtual void onWidgetLongClicked() {
+        // todo make widget slightly jiggly and move/resizeable
+    }
+
+public:
+    Clock(): type(UI), sharing(false), version("0.1"), description("Simple clock with widget"), name("Clock"), widget(new ClockWidget(onWidgetClicked, onWidgetLongClicked)) {
+
+    }
+
+    bool work(const context &ctx) override {
+
+    }
+};
+
+extern "C" Clock* create() {
+    return new Clock();
 };
 
 
