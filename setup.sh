621bb92f0dc801b3d80e0bc86ce317282d278828
Pascal Eberlein <pascal@eberlein.io>
2020-12-29 20:52:00 +0100

    update

--- setup.sh
+++ setup.sh
@@ -9,6 +9,21 @@ function install_taskflow_git() {
   cmake ..
   make -j $(nproc)
   sudo make install
+  cd /tmp/
+  rm -rf taskflow
+}
+
+function install_raylib_git() {
+  cd /tmp/
+  git clone https://github.com/raysan5/raylib
+  cd raylib
+  mkdir build
+  cd build
+  cmake ..
+  make -j$(nproc)
+  sudo make install
+  cd /tmp/
+  rm -rf raylib
 }
 
 if [ -f /sbin/apk ]; then
@@ -16,7 +31,8 @@ if [ -f /sbin/apk ]; then
 fi
 
 if [ -f /usr/bin/apt ]; then
-  sudo apt install -y cmake build-essential gcc g++ git
+  sudo apt install -y cmake build-essential gcc g++ git libx11-dev libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libgl-dev
 fi
 
-install_taskflow_git
+if [ ! -d /usr/local/include/taskflow ]; then install_taskflow_git; fi
+if [ ! -f /usr/local/include/raylib.h ]; then install_raylib_git; fi
