5ffd22fdc5a5f8efd7f59c739fa730691402fcf1
Pascal Eberlein <pascal@eberlein.io>
2021-01-04 22:39:48 +0100

    compile and install raylib on alpine as well

--- setup.sh
+++ setup.sh
@@ -19,31 +19,21 @@ function install_raylib_git() {
   cd raylib
   mkdir build
   cd build
-  cmake ..
+  cmake .. -DWITH_PIC=ON $1
   make -j$(nproc)
   sudo make install
   cd /tmp/
   rm -rf raylib
 }
 
-function install_raylib_git() {
-  cd /tmp/
-  git clone https://github.com/raysan5/raylib
-  cd raylib
-  mkdir build
-  cd build
-  cmake .. -DWITH_PIC=ON
-  make -j $(nproc)
-  sudo make install
-}
-
 if [ -f /sbin/apk ]; then
-  sudo apk add gcc g++ cmake make raylib-wayland-dev git
+  sudo apk add gcc g++ cmake make git
+  if [ ! -f /usr/local/include/raylib.h ]; then install_raylib_git "-DUSE_WAYLAND=ON"; fi
 fi
 
 if [ -f /usr/bin/apt ]; then
   sudo apt install -y cmake build-essential gcc g++ git libx11-dev libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libgl-dev libqgpsmm-dev
-  if [ ! -f /usr/local/include/raylib.h ]; then install_raylib_git; fi
+  if [ ! -f /usr/local/include/raylib.h ]; then install_raylib_git ""; fi
 fi
 
 if [ ! -d /usr/local/include/taskflow ]; then install_taskflow_git; fi
