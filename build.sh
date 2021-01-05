400fbc1803d529a71dc5f83e96b2ebbb1d5596b5
Pascal Eberlein <pascal@eberlein.io>
2021-01-05 02:09:00 +0100

    update

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 CMAKE_OPTIONS=""
 
-if [ $1 -eq "pp" ]; then CMAKE_OPTIONS="-DPINEPHONE=ON -DMODULES=ON"; fi
+if [ $1 = "pp" ]; then CMAKE_OPTIONS="-DPINEPHONE=ON -DMODULES=ON"; fi
 
 if [ ! -d build ]; then mkdir build; fi
 cd build
